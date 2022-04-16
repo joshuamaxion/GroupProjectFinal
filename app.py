9e7afd06a9391c3de55cabb41aca1225ab11a8a6
ET <ettionestuckeyii@gmail.com>
2022-04-15 22:41:32 -0700

    First ET commit

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import os
 from itsdangerous import URLSafeTimedSerializer, SignatureExpired
 from dotenv import load_dotenv,find_dotenv
 load_dotenv(find_dotenv())
+#importing map/weather data
+from folium import Map, Marker, Popup
+from geo import Geopoint
 
 
 # PROD configs
@@ -22,6 +25,37 @@ s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
 TokenTimer = 300
 
 db = SQLAlchemy(app)
+
+#Get latitude and longitude values
+locations = [[41, -1], [34.554296, -84.250793]]
+
+#Folium Map Instance
+mymap = Map(location = [40, 2])
+
+for loc in locations:
+    
+    #Getting locatin names
+    #created a geopoint instance
+    geopoint = Geopoint(latitude = loc[0], longitude = loc[1])
+    forecast = geopoint.get_weather()
+    popup_content = f"""
+    {forecast[0][0][-8:-6]}h: {round(forecast[0][1])}°F<img src= "http://openweathermap.org/img/wn/{forecast[0][-1]}@2x.png" width = 35>
+    <hr style="margin:1px">
+    {forecast[1][0][-8:-6]}h: {round(forecast[1][1])}°F<img src= "http://openweathermap.org/img/wn/{forecast[1][-1]}@2x.png" width = 35>
+    <hr style="margin:1px">
+    {forecast[2][0][-8:-6]}h: {round(forecast[2][1])}°F<img src= "http://openweathermap.org/img/wn/{forecast[2][-1]}@2x.png" width = 35>
+    <hr style="margin:1px">
+    {forecast[3][0][-8:-6]}h: {round(forecast[3][1])}°F<img src= "http://openweathermap.org/img/wn/{forecast[3][-1]}@2x.png" width = 35>
+    <hr style="margin:1px">
+    <p>Example popup</p>
+    """
+    #Create Popup object and addit to Geopoint
+    popup = Popup(popup_content, max_width=400)
+    popup.add_to(geopoint)
+    geopoint.add_to(mymap)
+
+#save as HTML file
+mymap.save("templates/map.html")
 
 class Users(db.Model):
     # Users table definition
@@ -61,7 +95,7 @@ def addUser(username, email):
 @app.route("/")
 def home():
     # main landing page; renders the main page if logged in, login page otherwise.
-    return render_template("index.html")
+    return render_template("map.html")
 
 @app.route('/login', methods = ['GET','POST'])
 def login():
